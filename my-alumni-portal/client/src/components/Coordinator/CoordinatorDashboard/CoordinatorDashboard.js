b9fb836f6cd57d1e205e3d5f54dca4f203c5b3c3
prathmesh patil <prathmeshpatil@Prathmeshs-MacBook-Air.local>
2022-05-11 13:21:53 +0530

    Added Coordinator Dashboard

--- my-alumni-portal/client/src/components/Coordinator/CoordinatorDashboard/CoordinatorDashboard.js
+++ my-alumni-portal/client/src/components/Coordinator/CoordinatorDashboard/CoordinatorDashboard.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import { React, useEffect} from 'react';
+import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function CoordinatorDashboard() {
-  return (
-    <div>CoordinatorDashboard</div>
-  )
+    let navigate = useNavigate();
+
+    useEffect(() => {
+      if(localStorage.getItem("Type")!="Coordinator") {
+        navigate("/");
+      }
+    })
+  
+    const logout = () => {
+      localStorage.removeItem("PRN");
+      localStorage.removeItem("Name");
+      localStorage.removeItem("Type");
+      navigate('/login');
+    }
+  
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="/">Coordinator Portal</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#features">Chat System</Nav.Link>
+          <Nav.Link href="/viewInternships">Internship Opportunities</Nav.Link>
+          <Nav.Link href="/viewJobs">Job Opportunities</Nav.Link>
+          <Nav.Link href="/viewEvents">Events and Workshops</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">My Profile</Nav.Link>
+          <Nav.Link onClick={logout}>Logout</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
 }
 
 export default CoordinatorDashboard
