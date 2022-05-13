bea2922294823e02e6df562a8448b8aec5047492
shailesh0811 <shailesh.mohite7543@gmail.com>
2022-05-13 16:59:58 +0530

    updateStudentProfile

--- my-alumni-portal/client/src/components/Profiles/ProfileChange/UpdateStudentProfile.js
+++ my-alumni-portal/client/src/components/Profiles/ProfileChange/UpdateStudentProfile.js
@@ -1,6 +1,16 @@
-import { React, useState} from 'react';
+import { React, useState } from 'react';
 import Axios from "axios";
 import { useNavigate } from 'react-router-dom';
+import pic4 from "../../../kit.png";
+import Form from 'react-bootstrap/Form';
+import pic from "../../../HighlightsAlumni.png";
+import pic1 from "../../../kit.png";
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+import './UpdateStudentProfile.css';
+
+
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function UpdateStudentProfile() {
 
@@ -10,7 +20,7 @@ function UpdateStudentProfile() {
   const [department, setDepartment] = useState("");
   const [current_study_year, setCurrentStudyYear] = useState();
   const [passout_year, setPassoutYear] = useState();
-  const [contact_number, setContactNumber] = useState(""); 
+  const [contact_number, setContactNumber] = useState("");
   const [email_id, setEmailId] = useState("");
   const [linkdin_profile, setLinkdinProfile] = useState("");
   let navigate = useNavigate();
@@ -19,146 +29,197 @@ function UpdateStudentProfile() {
     e.preventDefault();
     let toReq = 'http://localhost:3001/updateStudentDetail/';
     let obj = {
-        PRN: localStorage.getItem("PRN"),
-        name: localStorage.getItem("Name"),
-        date_of_birth: date_of_birth,
-        gender: gender,
-        year_of_admission: year_of_admission,
-        department: department,
-        current_study_year: current_study_year,
-        passout_year: passout_year,
-        contact_number: contact_number,
-        email_id: email_id,
-        linkdin_profile: linkdin_profile
+      PRN: localStorage.getItem("PRN"),
+      name: localStorage.getItem("Name"),
+      date_of_birth: date_of_birth,
+      gender: gender,
+      year_of_admission: year_of_admission,
+      department: department,
+      current_study_year: current_study_year,
+      passout_year: passout_year,
+      contact_number: contact_number,
+      email_id: email_id,
+      linkdin_profile: linkdin_profile
     }
 
     console.log(obj);
 
     const headers = {
-        'Content-Type':'application/json'
+      'Content-Type': 'application/json'
     }
 
     Axios.put(toReq, obj, {
-        headers:headers
+      headers: headers
     }).then(() => {
-        document.getElementById("updatedDetails").innerHTML = "Updated Details Sucessfully";
-        document.getElementById("updatedDetails").style.color = "green";
+      document.getElementById("updatedDetails").innerHTML = "Updated Details Sucessfully";
+      document.getElementById("updatedDetails").style.color = "green";
     })
   }
 
   const handleGenderChange = (event) => {
-      setGender(event.target.value);
+    setGender(event.target.value);
   }
 
   const handleDepartmentChange = (event) => {
-      setDepartment(event.target.value);
+    setDepartment(event.target.value);
   }
 
   const goToDashboard = () => {
-    if(localStorage.getItem("Type")=="Admin") {
-        navigate('/adminDashboard');
-    } else if (localStorage.getItem("Type")=="Student") {
-        navigate('/studentDashboard');
-    } else if (localStorage.getItem("Type")=="Alumni") {
-        navigate("/alumniDashboard");
+    if (localStorage.getItem("Type") == "Admin") {
+      navigate('/adminDashboard');
+    } else if (localStorage.getItem("Type") == "Student") {
+      navigate('/studentDashboard');
+    } else if (localStorage.getItem("Type") == "Alumni") {
+      navigate("/alumniDashboard");
     } else {
-        navigate("/coordinatorDashboard");
+      navigate("/coordinatorDashboard");
     }
-}
+  }
+
+
+  const logout = () => {
+    localStorage.removeItem("PRN");
+    localStorage.removeItem("Name");
+    localStorage.removeItem("Type");
+    navigate('/login');
+  }
 
   return (
     <>
-    <div className='updateProfileForm'>
-    <h2>Update Profile</h2>
-    <form onSubmit={handleSubmit}>
-      <label>
-        <p>PRN</p>
-        <input type="text" placeholder={localStorage.getItem("PRN")} disabled={true}/>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Name</p>
-        <input type="text" placeholder={localStorage.getItem("Name")} disabled={true}/>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Date of Birth</p>
-        <input type="date" onChange={e => setDateOfBirth(e.target.value)}/>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Gender</p>
-        <select onChange={handleGenderChange}>
-            <option value="NULL">Select Option</option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-        </select>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Year of Admission</p>
-        <input type="text" onChange={e => setAdmissionYear(e.target.value)}/>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Current Study Year</p>
-        <input type="text" onChange={e => setCurrentStudyYear(e.target.value)}/>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Expected Passing Year</p>
-        <input type="text" onChange={e => setPassoutYear(e.target.value)}/>
-      </label>
-      <br />
-      <br />
+    <div className='bgcolor'>
 
-      <label>
-        <p>Department</p>
-        <select onChange={handleDepartmentChange}>
-            <option value="NULL">Select Option</option>
-            <option value="CSE">Computer Science and Engineering</option>
-            <option value="ENTC">Electronics and Telecommunication</option>
-            <option value="MECH">Mechanical Engineering</option>
-            <option value="ELE">Electrical Engineering</option>
-            <option value="BIO">Biotech</option>
-            <option value="CIVIL">Civil Engineering</option>
-            <option value="ENV">Environment Engineering</option>
-            <option value="AI">Artificial Intelligence</option>
-            <option value="DS">Data Science</option>
-        </select>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Contact Number</p>
-        <input type="text" onChange={e => setContactNumber(e.target.value)}/>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>Email ID</p>
-        <input type="text" onChange={e => setEmailId(e.target.value)}/>
-      </label>
-      <br />
-      <br />
-      <label>
-        <p>LinkedIn Profile Link</p>
-        <input type="text" onChange={e => setLinkdinProfile(e.target.value)}/>
-      </label>
-      <div>
-        <div id='updatedDetails'></div>
-        <button className='submitbtn' type="submit">Submit</button>
+      <div className="jumbotron jumbotron-billboard">
+        <div className="img"></div>
+        <div className="container">
+          <div className="row">
+           
+            <div>
+              
+              
+            <Navbar collapseOnSelect expand="lg" >
+                  <Container>
+              <div className="col-lg-2"><img src={pic4} className="img-fluid rounded thumbnail-image" /> </div>    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  
+
+                    <Navbar.Brand href="/"><b>Alumni Portal</b> </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                      <Nav className="me-auto">
+                        <Nav.Link href="#features"><b>Chat System</b> </Nav.Link>
+                        <Nav.Link href="/viewInternships"><b>Internship Opportunities</b> </Nav.Link>
+                        <Nav.Link href="/viewJobs"><b>Job Opportunities</b> </Nav.Link>
+                        <Nav.Link href="/viewEvents"><b>Events and Workshops</b> </Nav.Link>
+                      </Nav>
+                      <Nav>
+                        <Nav.Link href="/updateStudentProfile">My Profile</Nav.Link>
+                        <Nav.Link onClick={logout}>Logout</Nav.Link>
+                      </Nav>
+                    </Navbar.Collapse>
+                  </Container>
+                </Navbar>
+            </div>
+          </div>
+        </div>
       </div>
-    </form>
+
+
+      <div className="box-form">
+
+
+
+        <div className="right">
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label><b>PRN</b></Form.Label>
+              <Form.Control type="text" placeholder={localStorage.getItem("PRN")} disabled={true} />
+
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Name</b></Form.Label>
+              <Form.Control type="text" placeholder={localStorage.getItem("Name")} disabled={true} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Date of Birth</b></Form.Label>
+              <Form.Control type="date" onChange={e => setDateOfBirth(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Gender</b></Form.Label> <br />
+              <select onChange={handleGenderChange}>
+                <option value="NULL">Select Option</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Year of Admission</b></Form.Label>
+              <Form.Control type="text" onChange={e => setAdmissionYear(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Current Study Year</b></Form.Label>
+              <Form.Control type="text" onChange={e => setCurrentStudyYear(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Expected Passing Year</b></Form.Label>
+              <Form.Control type="text" onChange={e => setAdmissionYear(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Department</b></Form.Label>
+              <select onChange={handleDepartmentChange}>
+                <option value="NULL">Select Option</option>
+                <option value="CSE">Computer Science and Engineering</option>
+                <option value="ENTC">Electronics and Telecommunication</option>
+                <option value="MECH">Mechanical Engineering</option>
+                <option value="ELE">Electrical Engineering</option>
+                <option value="BIO">Biotech</option>
+                <option value="CIVIL">Civil Engineering</option>
+                <option value="ENV">Environment Engineering</option>
+                <option value="AI">Artificial Intelligence</option>
+                <option value="DS">Data Science</option>
+              </select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Contact Number</b></Form.Label>
+              <Form.Control type="text" onChange={e => setContactNumber(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Email ID</b></Form.Label>
+              <Form.Control type="text" onChange={e => setEmailId(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label><b>Linkdin Profile Link </b></Form.Label>
+              <Form.Control type="text" onChange={e => setEmailId(e.target.value)} />
+            </Form.Group>
+
+            <div className='button'>
+              <div id='wrongIDorPass'></div>
+              <Button variant="success" className='submitbtn' type="submit">Update Profile</Button>{' '} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Button variant="primary" onClick={goToDashboard}>Go back to Dashboard</Button>{' '}
+              <br />
+
+
+            </div>
+          </Form>
+          <br />
+          <br />
+
+          
+        </div>
+
+      </div><br />
+
     </div>
-    <br />
-    <button onClick={goToDashboard}>Go back to Dashboard</button>
+
+
     </>
   )
 }
